784c0adfa9a332aadf219c0baaa5018a8cd89881
Priyanshi Dharu <108050901+Priyanshi1017@users.noreply.github.com>
2022-07-17 15:23:21 +0930

    Update SQLQuery_PortfolioProject_CovidData.sql

--- SQLQuery_PortfolioProject_CovidData.sql
+++ SQLQuery_PortfolioProject_CovidData.sql
@@ -115,3 +115,47 @@ FROM CovidDeaths dea join CovidVaccinations vac
 where dea.continent is not null 
 
 select *,cast(RollingPeopleVaccinated as float)/population *100 as Rollingpeoplevacciated_percentage from #populationvaccinated
+
+--creating view to display maximumpeoplevaccinated in each location/country
+
+create View Maximumpeoplevaccinated as
+  select date,location,people_vaccinated,sum(cast(people_vaccinated as bigint)) over (PARTITION by location order by date) as Maximumpeoplevaccinated
+ from CovidVaccinations
+ where people_vaccinated is not null  
+ 
+ -- Total New Cases for each month in India
+
+select year(date) as Year,month(date) as Month,sum(new_cases) as Total_Case
+from CovidDeaths 
+where location = 'India'
+group by year(date),month(date)
+order by 1
+
+--- storing a procedure which returns Total New Cases for Each Location when given Location as Parameter
+ALTER PROCEDURE NewCasesForEachMonth
+@Location nvarchar(100)
+AS
+select YEAR(date) as Year, Month(date) as Month,location,sum(new_cases)
+from CovidDeaths
+where new_cases is not null and location = @Location
+group by YEAR(date),Month(date),location
+order by 1,2
+--- can also be created and stored as view to create barchart for tracking new cases in each month
+EXEC NewCasesForEachMonth @Location= 'Spain'
+
+--- getting new first doses and new second doses on each day for each location 
+select new_vaccinations,people_fully_vaccinated,lag(people_fully_vaccinated) over (order by date ) as stepdown,
+---- for newfirstdoses in India using lag() function
+case
+when lag(people_fully_vaccinated) over (order by date) is null and people_fully_vaccinated is null then new_vaccinations
+when lag(people_fully_vaccinated) over (order by date) is null and people_fully_vaccinated is not null then cast(new_vaccinations as int) - cast(people_fully_vaccinated as int)
+when lag(people_fully_vaccinated) over (order by date) is not null
+then cast(new_vaccinations as int) - (cast(people_fully_vaccinated as int) - cast(lag(people_fully_vaccinated) over (order by date ) as int))
+end as NewFirstDoses,
+----- for newseconddoses in India 
+case 
+when lag(people_fully_vaccinated) over (order by date)  is not null then
+cast(people_fully_vaccinated as int)-cast(lag(people_fully_vaccinated) over (order by date ) as int)
+when lag(people_fully_vaccinated) over (order by date) is null then people_fully_vaccinated
+end as NewSecondDoses
+from CovidVaccinations where location = 'India'
